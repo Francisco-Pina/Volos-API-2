c0f795485e0a17893182b0fd7419c00f6c2a8934
Francisco-Pina <francisco.ppina@hotmail.com>
2022-06-24 11:35:54 -0400

    Cleaned API

--- volosAPI.py
+++ volosAPI.py
@@ -5,215 +5,171 @@ Volos Python API
 Documentation at: https://github.com/Francisco-Pina/Volos-API-2
 """
 
-__author__ = 'Francisco Pina'
-__copyright__ = '2022 Volos Portfolio Solutions, LLC.. All rights reserved'
+__author__ = 'Saif Sultan'
+__copyright__ = '2022 Volos Portfolio Solutions, Inc.. All rights reserved'
 __version__ = '2.0'
 __interpreter__ = 'Anaconda - Python 3.9.5 64 bit'
 __maintainer__ = 'Francisco Pina'
 __email__ = 'francisco.pina@volossoftware.com'
 __status__ = 'In Progress'
 
+
+
 import json
 import requests
 import pandas as pd
+import io
+import os
+import datetime
 
 
-class VolosAPI:
+class volosAPI(object):
+    API_KEY = None
+    API_STAGE = 'prod'
+    API_ENDPOINT = 'https://api-data-ci-w1.volossoftware.com/'
 
-    def __init__(self, volos_api_key=None):
-        # create function to test API key
-        self.volos_api_key = volos_api_key
-        # by default, the api will be set to strategy
-        self.api_endpoint = 'https://api-data-ci-w1.volossoftware.com/ci'
+    def __init__(self, api_key):
+        self.API_KEY = api_key
+        self.API_ENDPOINT = 'https://api-data-ci-w1.volossoftware.com/'
 
-    def set_strategy_api(self):
-        self.api_endpoint = 'https://api-data-ci-w1.volossoftware.com/ci'
+    def set_api_key(self, api_key):
+        self.API_KEY = api_key
 
-    def set_index_api(self):
-        self.api_endpoint = 'https://api-server-prod-e2.volossoftware.com'
+    def switch_stage_prod(self):
+        self.API_STAGE = 'prod'
 
-    def get_url(self, endpoint):
-        return self.api_endpoint + endpoint
+    def switch_stage_ci(self):
+        self.API_STAGE = 'ci'
 
-    def try_index_first(self, strategy_id):
-        index_df = self.get_info_public_indexes()
-        index_strat_id = None
+    def switch_stage_ir(self):
+        self.API_STAGE = 'ir'
 
-        if strategy_id in index_df['strategy_id'].tolist() or strategy_id in index_df['index_ticker'].tolist():
-            try:
-                index_id = index_df.loc[index_df['strategy_id'] == strategy_id, 'index_id'].item()
-                index_strat_id = index_df.loc[index_df['strategy_id'] == strategy_id, 'strategy_id'].item()
+    def get_headers(self):
+        if self.API_KEY is None:
+            raise Exception("Please set API Key")
+        return {'x-api-key': self.API_KEY}
 
-            except:
-                index_id = index_df.loc[index_df['index_ticker'] == strategy_id, 'index_id'].item()
-                index_strat_id = index_df.loc[index_df['index_ticker'] == strategy_id, 'strategy_id'].item()
-            return True, index_id, index_strat_id
-
+    def get_url(self, uri, api_stage=None):
+        if api_stage is None:
+            return self.API_ENDPOINT + self.API_STAGE + uri
         else:
-            return False, strategy_id, index_strat_id
+            return self.API_ENDPOINT + api_stage + uri
 
-    def get_time_series_index(self, index_id):
-        self.set_index_api()
-        endpoint = '/index/prod/index/get_time_series'
-        url = self.get_url(endpoint=endpoint)
+    def get_strategy_total_returns(self, strategy_id, output_format='csv', start_date='1990-01-01',
+                                   end_date='2050-01-01'):
+        uri = '/time-series/totalreturns'
+        payload = {'strategy_id': strategy_id, 'output_format': output_format, 'start_date': start_date,
+                   'end_date': end_date}
 
-        obj = {"index_id": index_id}
-        headers = {"content-type": "json"}
-        x = requests.post(url, data=json.dumps(obj), headers=headers)
-
-        index_df = pd.json_normalize(x.json())
-        index_df['Strategy_id'] = index_id
-
-        return index_df
-
-    def get_time_series(self, strategy_id):
-
-        test_true_false, test_index, _ = self.try_index_first(strategy_id)
-        df = None
-
-        if not test_true_false:
-            try:
-                self.set_strategy_api()
-                endpoint = '/validation/get-validation-df'
-                url = self.get_url(endpoint=endpoint)
-
-                obj = {"strategy_id": strategy_id}
-                headers = {"x-api-key": self.volos_api_key}
-
-                output = requests.post(url, data=json.dumps(obj), headers=headers).json()
-
-                if list(output.keys())[0] == 'validation_csv_url':
-                    df = pd.read_csv(output['validation_csv_url'])
-                else:
-                    df = pd.json_normalize(output)
-
-                df = df.loc[:, ['date', 'index_value']]
-                df.columns = ['date', 'series_value']
-                df.dropna().reset_index(drop=True)
-                df['Strategy_id'] = strategy_id
-                df = df.loc[:, ['date', 'series_value', 'Strategy_id']]
-
-            except ValueError as e:
-                return {'Error': str(e)}
-
-        elif test_true_false:
-            ####
-            df = self.get_time_series_index(test_index)
-
-        return df
-
-    def add_drawdown(self, df):
-        list_vals = []
-        list_max = []
-        for i in df['series_value']:
-            list_vals.append(i)
-            current_max = max(list_vals)
-            list_max.append(current_max)
-        df['Peak'] = list_max
-        df['Drawdown'] = df['series_value'] - df['Peak']
-        df['Drawdown_Percent'] = df['Drawdown'] / df['Peak']
-
-        return df.loc[:, ['date', 'series_value', 'Strategy_id', 'Drawdown', 'Drawdown_Percent']]
-
-    def get_metrics(self, strategy_id):
-
-        test_true_false, test_index, index_strat_id = self.try_index_first(strategy_id)
-        df = None
-
-        if not test_true_false:
-            self.set_strategy_api()
-            endpoint = '/time-series/metrics'
-            url = self.get_url(endpoint=endpoint)
-
-            obj = {"strategy_id": strategy_id}
-            headers = {"x-api-key": self.volos_api_key}
-
-            x = requests.post(url, data=json.dumps(obj), headers=headers)
-            output = 'date' + x.text
-            data = [y.split(",") for y in output.split("\n")]
-            df = pd.DataFrame(data[1:], columns=data[0]).dropna().tail(1).reset_index(drop=True)
-            df.insert(1, 'Strategy_id', strategy_id)
-
-        elif test_true_false:
-            self.set_index_api()
-            endpoint = '/index_summary/prod/index_summary/metrics'
-            url = self.get_url(endpoint=endpoint)
-
-            obj = {"strategy_id": index_strat_id}
-            headers = {"content-type": "text/plain"}
-            x = requests.post(url, data=json.dumps(obj), headers=headers)
-
-            df = pd.json_normalize(x.json()['metrics'])
-            df.insert(0, 'date', pd.Timestamp.today().strftime('%Y-%m-%d'))
-            df.insert(1, 'Strategy_id', index_strat_id)
-
-            ######## Columns to Percent ########
-            cols_to_percentage = ['Annual_Returns', 'Downside_Risk', 'Sortino_Ratio',
-                                  'Annual_Volatility', 'Max_Drawdown', 'Tail_Ratio', 'CAGR',
-                                  'Profit_Prob', 'Calmar_Ratio', 'Cum_Returns_Final']
-            df.loc[:, cols_to_percentage] = df.loc[:, cols_to_percentage].div(100, axis=0)
-
-        return df.loc[:, ['date', 'Strategy_id', 'Annual_Returns', 'Annual_Volatility', 'CAGR',
-                          'Calmar_Ratio', 'Cum_Returns_Final', 'Downside_Risk', 'Max_Drawdown',
-                          'Profit_Prob', 'Sharpe_Ratio', 'Sortino_Ratio', 'Tail_Ratio']]
-
-    def get_strategy_details(self, strategy_id):
-
-        test_true_false, test_index, index_strat_id = self.try_index_first(strategy_id)
-        df = None
-
-        if not test_true_false:
-            # not working :(
-            # find how to use this endpoint
-            pass
-
-        elif test_true_false:
-            self.set_index_api()
-            df = self.get_info_public_indexes().loc[df['index_id'] == index_strat_id]
-
-        return df
-
-    def get_validation_data(self, strategy_id):
-        self.set_strategy_api()
-        endpoint = '/validation/get-validation-df'
-        url = self.get_url(endpoint=endpoint)
-
-        obj = {"strategy_id": strategy_id}
-        headers = {"x-api-key": self.volos_api_key}
-
-        output = requests.post(url, data=json.dumps(obj), headers=headers).json()
-
-        if list(output.keys())[0] == 'validation_csv_url':
-            df = pd.read_csv(output['validation_csv_url'])
+        res = requests.get(self.get_url(uri), params=payload, headers=self.get_headers())
+        if output_format == 'json':
+            return json.loads(res.content)
+        elif output_format == 'csv':
+            return pd.read_csv(io.StringIO(res.content.decode('utf-8')))
         else:
-            df = pd.json_normalize(output)
+            raise NotImplementedError("Incorrect output format")
 
-        df.dropna().reset_index(drop=True)
-        df['Strategy_id'] = strategy_id
+    def get_strategy_list_total_returns(self, strategy_id_list, start_date='1990-01-01', end_date='2050-01-01'):
+        uri = '/time-series/total-returns-multi'
+        payload = {'strategy_id_list': strategy_id_list, 'start_date': start_date, 'end_date': end_date}
+        res = requests.post(self.get_url(uri), data=json.dumps(payload), headers=self.get_headers())
+        return pd.read_csv(io.StringIO(res.content.decode('utf-8'))).rename(columns={'Unnamed: 0': 'date'})
 
-        return df
+    def get_strategy_metrics(self, strategy_id, start_date='1990-01-01', end_date='2050-01-01'):
+        uri = '/time-series/metrics'
+        payload = {'strategy_id': strategy_id, 'start_date': start_date, 'end_date': end_date}
+        res = requests.post(self.get_url(uri), data=json.dumps(payload), headers=self.get_headers())
+        return pd.read_csv(io.StringIO(res.content.decode('utf-8'))).rename(columns={'Unnamed: 0': 'date'})
 
-    def get_annual_returns(self, df):
-        date_series = pd.to_datetime(df['date'])
-        df = df.loc[date_series.dt.is_year_end].reset_index(drop=True)
-        df['annual_return'] = df['series_value'].pct_change()
-        df = df.dropna().reset_index(drop=True)
-        return df.loc[:, ['date', 'annual_return', 'Strategy_id']]
+    def get_strategy_trade_logs(self, strategy_id, start_date='1990-01-01', end_date='2050-01-01'):
+        uri = '/strategy/tradelogs'
+        payload = {'strategy_id': strategy_id, 'start_date': start_date, 'end_date': end_date}
+        res = requests.post(self.get_url(uri), data=json.dumps(payload), headers=self.get_headers())
+        return pd.read_csv(json.loads(io.StringIO(res.content.decode('utf-8')).getvalue())['tradelogs_csv_url'], index_col=[0])
 
-    def save_to_csv(self, df, name):
-        """Saves file to current folder"""
-        return df.to_csv(name + '.csv', index=False)
+    def get_strategy_positions(self, strategy_id, on_date, api_stage=None):
+        uri = '/strategy/positions'
+        payload = {'strategy_id': strategy_id, 'on_date': on_date}
+        res = requests.post(self.get_url(uri, api_stage=api_stage), data=json.dumps(payload),
+                            headers=self.get_headers())
+        return json.loads(io.StringIO(res.content.decode('utf-8')).getvalue())['post_strategy_positions_csv_url']
 
-    def trim_dates(self, df, start_date, end_date):
-        """Format: YYYY-MM-DD
-        Function includes start and end date"""
-        return df[(df['date'] >= start_date) & (df['date'] <= end_date)]
+    def get_strategy_list_meta_data(self, strategy_id_list):
+        uri = '/strategy/meta-data'
+        payload = {'strategy_id_list': strategy_id_list}
+        res = requests.post(self.get_url(uri), data=json.dumps(payload), headers=self.get_headers())
+        return json.loads(res.content.decode('utf-8'))
+
+    def get_strategy_list_tags(self, strategy_id_list):
+        uri = '/strategy/tags'
+        payload = {'strategy_id_list': strategy_id_list}
+        res = requests.post(self.get_url(uri), data=json.dumps(payload), headers=self.get_headers())
+        return json.loads(res.content.decode('utf-8'))
+
+    def get_strategy_excel_sheet(self, strategy_id):
+        uri = '/strategy/excel-sheet'
+
+        payload = {'strategy_id': strategy_id}
+        res = requests.get(self.get_url(uri), params=payload, headers=self.get_headers())
+        return res.url
+        # return pd.read_excel(io.StringIO(res.content))
+
+    def search_by_ticker(self, ticker):
+        uri = '/search/ticker'
+        payload = {'ticker': ticker}
+        res = requests.post(self.get_url(uri), data=json.dumps(payload), headers=self.get_headers())
+        return pd.read_csv(io.StringIO(res.content.decode('utf-8')))
+
+    def get_all_tags(self):
+        uri = '/misc/all-tags'
+        res = requests.get(self.get_url(uri), headers=self.get_headers())
+        return json.loads(res.content)
+
+    def get_timeseries_positions(self, strategy_id, start_date='1990-01-01', end_date='2050-01-01', api_stage=None):
+        uri = '/time-series/positions'
+        payload = {'strategy_id': strategy_id, 'start_date': start_date, 'end_date': end_date}
+        res = requests.post(self.get_url(uri, api_stage=api_stage), data=json.dumps(payload),
+                            headers=self.get_headers())
+        return pd.read_csv(io.StringIO(res.content.decode('utf-8')), index_col=[0])
+
+    def get_timeseries_positions_values(self, strategy_id, start_date='1990-01-01', end_date='2050-01-01',
+                                        api_stage=None):
+        uri = '/time-series/positions-values'
+        payload = {'strategy_id': strategy_id, 'start_date': start_date, 'end_date': end_date}
+        res = requests.post(self.get_url(uri, api_stage=api_stage), data=json.dumps(payload),
+                            headers=self.get_headers())
+        return pd.read_csv(io.StringIO(res.content.decode('utf-8')), index_col=[0])
+
+    def get_strategy_positions_meta_data(self, strategy_id, api_stage=None):
+        uri = '/strategy/positions/meta-data'
+        payload = {'strategy_id': strategy_id}
+        res = requests.post(self.get_url(uri, api_stage=api_stage), data=json.dumps(payload),
+                            headers=self.get_headers())
+        return pd.read_csv(io.StringIO(res.content.decode('utf-8')), index_col=[0])
+
+    def save_positions_to_excel(self, strategy_id, path='.', api_stage=None):
+
+        df_positions = self.get_timeseries_positions(strategy_id=strategy_id, api_stage=api_stage).pivot(index='date',
+                                                                                                         columns='security_id',
+                                                                                                         values='shares')
+        df_values = self.get_timeseries_positions_values(strategy_id=strategy_id, api_stage=api_stage).pivot(
+            index='date', columns='security_id', values='value')
+        df_meta = self.get_strategy_positions_meta_data(strategy_id=strategy_id, api_stage=api_stage)
+
+        sheet_name_list = ['holdings', 'values', 'meta_data']
+        df_list = [df_positions, df_values, df_meta]
+
+        timestamp = datetime.datetime.utcnow().strftime('%Y%m%d-%H%M%S-%f')
+        fname = os.path.join(path, '{}-{}.xlsx'.format(strategy_id, timestamp))
+        writer = pd.ExcelWriter(fname, engine='xlsxwriter')
+
+        for sheet_name, df in zip(sheet_name_list, df_list):
+            df.to_excel(writer, sheet_name=sheet_name)
+        writer.save()
+        print("saved to excel: {}".format(fname))
 
     def get_info_public_indexes(self):
-        self.set_index_api()
-        endpoint = '/index/prod/index/all_index_data'
-        url = self.get_url(endpoint=endpoint)
-
+        url = 'https://api-server-prod-e2.volossoftware.com/index/prod/index/all_index_data'
         headers = {"content-type": "json"}
         x = requests.post(url, headers=headers)
         index_df = pd.json_normalize(x.json())
@@ -222,19 +178,22 @@ class VolosAPI:
                                 'is_public', 'index_id', 'release_date']]
 
 
-if __name__ == "__main__":
-    temporary_file = open("../volos_api_key.txt", "rt")
-    volos_api_key = temporary_file.read()
-    temporary_file.close()
+if __name__ == '__main__':
+    strategy_id = 'your_strategy_id_here'
+    vs = volosAPI(api_key="your_api_key_here")
+    vs.switch_stage_ci()
+    print(vs.get_strategy_total_returns(strategy_id))
+    # print(vs.get_strategy_list_total_returns([strategy_id]))
+    # print(vs.get_strategy_metrics(strategy_id))
+    # print('trade_logs', vs.get_strategy_trade_logs(strategy_id))
+    # print('strat positions', vs.get_strategy_positions(strategy_id, on_date='2022-01-03'))
+    # print('list meta data', vs.get_strategy_list_meta_data([strategy_id]))
+    # print('list tags', vs.get_strategy_list_tags(['strategy_id']))
+    # print('excel sheet', vs.get_strategy_excel_sheet(strategy_id))
+    # print('search ticker', vs.search_by_ticker('VOCCQQQ95'))  # there's a bug here, query searches for ticker in tag_name and there is no such value
+    # print('get all tags', vs.get_all_tags())
+    # print('get timeseries positions values', vs.get_timeseries_positions_values(strategy_id))
+    # print('get strat pos metadata', vs.get_strategy_positions_meta_data(strategy_id))
+    # print('save positions to excel', vs.save_positions_to_excel(strategy_id))
+    # print(vs.get_info_public_indexes())
 
-    vs = VolosAPI(volos_api_key=volos_api_key)
-
-    strategy_id = 'VOCCGLD100'
-
-    df = vs.get_time_series(strategy_id)
-
-    df_metrics = vs.get_metrics(strategy_id)
-
-    # print(df)
-    print(df)
-    print(df_metrics)
